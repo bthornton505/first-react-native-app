0625b0e3cc4af91a209ae37acd98398551282a28
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-19 15:37:58 -0600

    imported Main Menu component to display it when app loads

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import MainMenu from './components/MainMenu'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi, Mariana!</Text>
+      <MainMenu />
     </View>
   );
 }
