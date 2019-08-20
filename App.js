5bb5d07b6a80a28faa19e1ba0d39df71347f4a6a
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-19 19:38:46 -0600

    added the MainMenu component and removed all flexbox styling

--- App.js
+++ App.js
@@ -2,21 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './components/Header'
+import MainMenu from './containers/MainMenu'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <MainMenu />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
-    flexDirection: 'row',
     backgroundColor: 'rgb(234, 34, 45)',
-    alignItems: 'flex-start',
     justifyContent: 'center',
   },
 });
