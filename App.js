06ca9b7a30a61b0acb620d7d6849fadd7cd393fc
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-20 18:43:31 -0600

    removed the components and decided to build main menu in one file for now"

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 import Header from './components/Header'
 import MainMenu from './containers/MainMenu'
@@ -7,8 +7,18 @@ import MainMenu from './containers/MainMenu'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header />
-      <MainMenu />
+
+      <View style={styles.headerStyle}>
+        <Text style={styles.menuText}>POKEDEX</Text>
+      </View>
+
+      <View style={styles.pokemonButton}>
+        <Button
+          onPress={() => alert('You clicked the button!')}
+          title="Press me"
+        />
+      </View>
+
     </View>
   );
 }
@@ -17,5 +27,21 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'rgb(234, 34, 45)',
     justifyContent: 'center',
+  },
+  headerStyle: {
+    backgroundColor: 'black',
+    padding: 20,
+  },
+  menuText: {
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+    paddingTop: 40,
+  },
+  pokemonButton: {
+    margin: 20,
+    padding: 15,
+    backgroundColor: '#000',
+    color: '#FFF'
   },
 });
