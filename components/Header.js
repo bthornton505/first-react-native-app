a64f003821ff756848247c8cb1d2fd830c0c34e4
Brenden Thornton <thorntonbrenden@gmail.com>
2019-08-25 14:39:30 -0600

    updated Header component to user the passed in props

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Header() {
+const Header = (props) => {
   return(
-    <View style={styles.headerStyle}>
-      <Text style={styles.menuText}>POKEDEX</Text>
-    </View>
+     <View style={props.style.headerContainer}>
+       {
+         props.setFont ? (
+           <Text style={props.style.header}>POKEDEX</Text>
+         ) : null
+       }
+     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  headerStyle: {
-    backgroundColor: 'black',
-    padding: 20,
-  },
-  menuText: {
-    color: 'white',
-    fontSize: 30,
-    textAlign: 'center',
-    paddingTop: 40,
-  }
-});
+export default Header;
