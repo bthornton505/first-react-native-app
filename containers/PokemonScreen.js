42f7d10d6906a17962b08fde072cbbcd8b7230f9
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-15 16:19:47 -0600

    created axios request for pulling pokemon data

--- containers/PokemonScreen.js
+++ containers/PokemonScreen.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import * as Font from 'expo-font';
 import Header from '../components/Header'
+import AllPokemon from '../components/AllPokemon';
+import axios from 'axios';
 
 class PokemonScreen extends Component {
   state = {
     fontLoaded: false,
+    pokemon: []
   };
 
   async componentDidMount() {
@@ -16,14 +19,23 @@ class PokemonScreen extends Component {
     this.setState({ fontLoaded: true });
 
     // Build axios request for pulling pokemon data
+    // let i = 0;
+    // do
+    //   axios.get(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+    //     .then(response => this.setState({ pokemon: response.data}), i += 1)
+    // while (i < 151)
+    axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=151/`)
+        .then(response => this.setState({ pokemon: response.data}))
   };
 
   render(){
+    // console.log(this.state.pokemon)
     return(
       <View>
         <Header style={styles} setFont={this.state.fontLoaded} />
 
         <View style={styles.pokemonContainer}>
+          <AllPokemon pokemon={this.state.pokemon} />
         </View>
       </View>
     )
